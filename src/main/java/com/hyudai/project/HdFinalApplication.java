59e3f06e4b56705b7fcebcdb57b341426ec2415d
Yeseong Jeong <80377632+YS-Jeong@users.noreply.github.com>
2022-06-14 15:42:38 +0900

    first commit

---
+++ src/main/java/com/hyudai/project/HdFinalApplication.java
@@ -0,0 +1,13 @@
+package com.hyudai.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdFinalApplication.class, args);
+	}
+
+}
