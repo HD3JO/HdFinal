bfa7ed9e15ecf7b580c71660d052534d8874c889
go-coding1 <rhtjrwnszyzy@gmail.com>
2022-06-15 16:30:42 +0900

    DataSourceTest 클래스 추가

---
+++ src/test/java/com/hyudai/project/persistence/DataSourceTests.java
@@ -0,0 +1,38 @@
+package com.hyudai.project.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DataSourceTests {	
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}//end static
+	
+	@Test
+	public void TestConnection() {
+		try {
+			Connection conproduct = 
+					DriverManager.getConnection(
+							"jdbc:oracle:thin:@123.142.252.25:40026/xepdb1"
+							,"productmanager","Handsome123#");
+			System.out.println(conproduct);
+			
+			Connection conuser = 
+					DriverManager.getConnection(
+							"jdbc:oracle:thin:@123.142.252.25:40026/xepdb1"
+							,"usermanager","Handsome123#");
+			System.out.println(conuser);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());			
+		}//end try			
+	}//end test
+}//end class
