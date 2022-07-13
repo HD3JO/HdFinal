ba996cacff793e286930408f24a25803ec1e2dcd
go-coding1 <rhtjrwnszyzy@gmail.com>
2022-07-13 17:15:14 +0900

    관리자 페이지 채팅 번역 추가
    관리자 페이지 채팅 번역 추가

--- src/main/java/com/hyundai/project/controller/TranslateController.java
+++ src/main/java/com/hyundai/project/controller/TranslateController.java
@@ -53,6 +53,57 @@ public class TranslateController {
 		return new ResponseEntity<>(responseBody, HttpStatus.OK);
 	}
 	
+	@RequestMapping(value="/transchtoko", method=RequestMethod.POST)
+	public ResponseEntity<String> transko(@RequestBody TransDTO transDTO){
+		String clientId = "ficy2Gd86GEzeotSAVYR";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "SwNtlzAhS6";//애플리케이션 클라이언트 시크릿값";
+
+        String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+        String text;
+        try {
+            text = URLEncoder.encode(transDTO.getBeforeM(), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("인코딩 실패", e);
+        }
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+
+        String responseBody = postCH(apiURL, requestHeaders, text);
+
+        System.out.println(responseBody);
+        
+		return new ResponseEntity<>(responseBody, HttpStatus.OK);
+	}
+	private static String postCH(String apiUrl, Map<String, String> requestHeaders, String text){
+        HttpURLConnection con = connect(apiUrl);
+        String postParams = "source=zh-CN&target=ko&text=" + text; //원본언어: 한국어 (ko) -> 목적언어: 영어 (en)
+        try {
+            con.setRequestMethod("POST");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            con.setDoOutput(true);
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(postParams.getBytes());
+                wr.flush();
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+                return readBody(con.getInputStream());
+            } else {  // 에러 응답
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+	
 	private static String post(String apiUrl, Map<String, String> requestHeaders, String text){
         HttpURLConnection con = connect(apiUrl);
         String postParams = "source=ko&target=zh-CN&text=" + text; //원본언어: 한국어 (ko) -> 목적언어: 영어 (en)
---
+++ src/main/java/com/hyundai/project/dto/TransDTO.java
@@ -0,0 +1,14 @@
+package com.hyundai.project.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class TransDTO {
+	private String beforeM;
+	private String afterM;
+
+}
